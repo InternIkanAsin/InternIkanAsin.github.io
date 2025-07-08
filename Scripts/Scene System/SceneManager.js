250088a69235ccd35f659920a0c14422020fd54c
MovingGrass <internikanasin@gmail.com>
2025-07-08 11:31:11 +0700

    cleanup scene manager

--- Scripts/Scene System/SceneManager.js
+++ Scripts/Scene System/SceneManager.js
@@ -8,22 +8,22 @@ export class SceneManager {
         this.scene.AudioManager.fadeOutMusic('cutsceneMusic', 1500);
 
         this.scene.cameras.main.once('camerafadeoutcomplete', () => {
-            // Bersihkan aset cutscene 1
+            
             this.scene.backgroundCutscene1?.destroy();
             this.scene.chosenBachelor?.destroy();
 
             this.scene.cameras.main.fadeIn(2000);
 
-            // Panggil fungsi untuk setup selection screen
+            
             if (typeof this.scene.createSelectionScreen === 'function') {
                 this.scene.createSelectionScreen();
             }
 
-            // Ganti musik dan fade in ke selection screen
+            
             this.scene.AudioManager.playMusic('minigameMusic');
             this.scene.AudioManager.fadeInMusic('minigameMusic', 1000);
             this.scene.cameras.main.fadeIn(2000);
-            // --- AKHIR LOGIKA TEKS ---
+            
         });
     }
 
