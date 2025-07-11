1604589d2c9cc1211ab41ddd2e2f0b864ea65ce8
Tristan Chandra <internikanasin@gmail.com>
2025-07-11 11:26:45 +0700

    downloaded github pages for deploy

--- DressupGame/vite.config.js
+++ DressupGame/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // Atur base path menjadi path relatif.
   // Ini adalah perbaikan paling penting.
-  base: './', 
+  base: '/InternIkanAsin.github.io/',
   build: {
     // Opsi tambahan untuk memastikan aset dimuat dengan benar
     assetsInlineLimit: 0,
