d6fd51228110164edd84bd3e68f8af5b1a28bc34
MovingGrass <internikanasin@gmail.com>
2025-08-20 14:02:12 +0700

    Update MiniGameManager.js

--- DressupGame/src/Scripts/Minigame/MiniGameManager.js
+++ DressupGame/src/Scripts/Minigame/MiniGameManager.js
@@ -826,7 +826,6 @@ export class MiniGameManager {
                 activeButton = targetButton;
                 activeButton.button.emit('pointerdown', pointer);
             } else if (!(hitObjects.includes(scene.scrollPanel) || hitObjects.some(h => scene.scrollPanel.contains?.(h)))) {
-                
                 event.stopPropagation();
             }
         });
